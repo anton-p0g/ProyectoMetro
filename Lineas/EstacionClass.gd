cccf5527c7c87c102bc2f4e2e5b01a735d0ccae7
wenjie198 <wenjie1987711563@gmail.com>
2024-11-01 13:40:35 +0100

    Ascensores int -> bool

--- Lineas/EstacionClass.gd
+++ Lineas/EstacionClass.gd
@@ -8,14 +8,14 @@ class_name EstacionClass
 
 var nombre: String
 var ID: String 
-var coordenadas: Array  # Primera posicion long, segunda lat
+var coordenadas: Array  # Primera posicion lat, segunda long
 var transbordos: Dictionary # Clave = Linea a la que conecta: Valor = ID de la estacion a la que conecta
 var adyacentes: Dictionary # Clave = ID de estacion a la que conecta: Valor = distancia
-var ascensores: int
+var ascensores: bool
 var escaleras: int
 
 func _init(p_nombre: String, p_ID: String, p_coordenadas: Array = [], p_transbordos: Dictionary = {},
-           p_adyacentes: Dictionary = {}, p_ascensores: int = 0, p_escaleras: int = 0):
+           p_adyacentes: Dictionary = {}, p_ascensores: bool = false, p_escaleras: int = 0):
     
     self.nombre = p_nombre
     self.ID = p_ID
@@ -23,5 +23,4 @@ func _init(p_nombre: String, p_ID: String, p_coordenadas: Array = [], p_transbor
     self.transbordos = p_transbordos
     self.adyacentes = p_adyacentes
     self.ascensores = p_ascensores
-    #self.horarios = p_horarios
     self.escaleras = p_escaleras
