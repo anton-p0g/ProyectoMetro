b28f5d70833b02ea924d212019c517938e9e1b38
wenjie198 <wenjie1987711563@gmail.com>
2024-11-01 15:20:15 +0100

    Cambiado estaciones lista -> diccionario

--- Lineas/LineaClass.gd
+++ Lineas/LineaClass.gd
@@ -12,45 +12,51 @@ const Estacion = preload("res://Lineas//EstacionClass.gd")  # Asegurate de que e
 
 var nombre: String
 var color: Color
-var estaciones: Array
+var estaciones: Dictionary
 var horarios: Dictionary # Clave = Día semana Valor = Hora
 var frecuencia: Dictionary # Clave = franja horaria : Valor = frecuencia en minutos
 
 # Constructor de la clase
-func _init(p_nombre: String, p_color: Color, p_horarios: Dictionary, p_frecuencia: Dictionary, p_estaciones: Array = []):
-    self.nombre = p_nombre
-    self.color = p_color
-    self.horarios = p_horarios
-    self.frecuencia = p_frecuencia
-    self.estaciones = p_estaciones
+func _init(p_nombre: String, p_color: Color, p_horarios: Dictionary, p_frecuencia: Dictionary, p_estaciones: Dictionary = {}):
+	self.nombre = p_nombre
+	self.color = p_color
+	self.horarios = p_horarios
+	self.frecuencia = p_frecuencia
+	self.estaciones = p_estaciones
 
 
 # Metodo para agregar una estacion a la linea
 func agregar_estacion(estacion: Estacion):
-    estaciones.append(estacion)
+	self.estaciones[estacion.ID] = estacion
 
 
 # Metodo para obtener una estacion por su ID
 func obtener_estacion_ID(ID: String) -> Estacion:
-    for estacion in estaciones:
-        if estacion.ID == ID:
-            return estacion
-    return null
+	return self.estaciones[ID]
 
 
 # Metodo para conectar dos estaciones
 func conectar_estaciones(ID1: String, ID2: String, distancia: float):
-    var estacion1 = obtener_estacion_ID(ID1)
-    var estacion2 = obtener_estacion_ID(ID2)
-
-    if estacion1 and estacion2:
-        estacion1.adyacentes[estacion2.ID] = distancia
-        estacion2.adyacentes[estacion1.ID] = distancia
-    else:
-        print("Error: Una o ambas estaciones no se encontraron")
+	var estacion1 = obtener_estacion_ID(ID1)
+	var estacion2 = obtener_estacion_ID(ID2)
+	
+	if estacion1 == null:
+		print("Estación con ID %s no encontrada" % ID1)
+		return
+	
+	if estacion2 == null:
+		print("Estación con ID %s no encontrada" % ID2)
+		return
+		
+	if !estacion1.adyacentes.has(estacion2.ID):
+		estacion1.adyacentes[estacion2.ID] = distancia
+		estacion2.adyacentes[estacion1.ID] = distancia
+		print("Conectadas: %s y %s a una distancia de %s minutos." % [estacion1.nombre, estacion2.nombre, distancia])
+	else:
+		print("%s ya tiene a %s como adyacente." % [estacion1.nombre, estacion2.nombre])
 
 
 # Metodo para mostrar todas las estaciones de una linea
 func pretty_print():
-    for estacion in estaciones:
-        print("Estacion: ", estacion.nombre, " - ID: ", estacion.ID)
+	for estacion in estaciones:
+		print("Estacion: ", estacion.nombre, " - ID: ", estacion.ID)
