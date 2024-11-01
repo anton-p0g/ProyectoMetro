0b31ec246dd2272d8592ba842d2a03c2a79062ba
eduvicgar <eduvg0102@gmail.com>
2024-11-01 13:26:35 +0100

    Cambio coordenadas

--- Lineas/LineaA.gd
+++ Lineas/LineaA.gd
@@ -11,14 +11,14 @@ func _ready():
     # El array de horarios se corresponde al horario del subte cada día de la semana
     # Las frecuencias se corresponden con el horario de apertura del subte (ejemplo: si abre de 05:30 a 22:53 la frecuencia es de 4 min)
     # En transbordos faltan los IDs de las estaciones, se completarán cuando sepa cuáles son esos IDs.
-    var plaza_de_mayo = EstacionClass.new("Plaza de Mayo", "PM", [-58.3709684989674, -34.6088103091689], {}, {"PE": 1}, true, 0)
-    var peru = EstacionClass.new("Perú", "PE", [-58.3742677264304,-34.6085590738532], {"D": "(ID de Catedral)", "E": "(ID de Bolívar)"}, {"PM": 1, "PI": 1}, true, 2)
-    var piedras = EstacionClass.new("Piedras", "PI", [-58.3790851530908,-34.608881721215], {}, {"PE": 1, "LI": 2}, {}, false, 1)
-    var lima = EstacionClass.new("Lima", "LI", [-58.3822324010181,-34.6090998065519], {"C": "(ID de Avda. de Mayo)"}, {"PI": 2, "SP": 1}, false, 1)
-    var saenz_pena = EstacionClass.new("Saenz Peña", "SP", [-58.3867771940873,-34.6094125865027], {}, {"LI": 1, "CO": 1}, false, 2)
-    var congreso = EstacionClass.new("Congreso", "CO", [-58.3926688246648,-34.6092256843174], {}, {"SP": 1, "PA": 1}, true, 2)
-    var pasco = EstacionClass.new("Pasco", "PA", [-58.3984269918123,-34.6096459617052], {}, {"CO": 1, "AL": 1}, false, 0)
-    var alberti = EstacionClass.new("Alberti", "AL", [-58.401207534233,-34.6098335784398], {}, {"PA": 1}, false, 0)
+    var plaza_de_mayo = EstacionClass.new("Plaza de Mayo", "PM", [-34.6088103091689, -58.3709684989674], {}, {"PE": 1}, true, 0)
+    var peru = EstacionClass.new("Perú", "PE", [-34.6085590738532, -58.3742677264304], {"D": "(ID de Catedral)", "E": "(ID de Bolívar)"}, {"PM": 1, "PI": 1}, true, 2)
+    var piedras = EstacionClass.new("Piedras", "PI", [-34.608881721215, -58.3790851530908], {}, {"PE": 1, "LI": 2}, {}, false, 1)
+    var lima = EstacionClass.new("Lima", "LI", [-34.6090998065519, -58.3822324010181], {"C": "(ID de Avda. de Mayo)"}, {"PI": 2, "SP": 1}, false, 1)
+    var saenz_pena = EstacionClass.new("Saenz Peña", "SP", [-34.6094125865027, -58.3867771940873], {}, {"LI": 1, "CO": 1}, false, 2)
+    var congreso = EstacionClass.new("Congreso", "CO", [-34.6092256843174, -58.3926688246648], {}, {"SP": 1, "PA": 1}, true, 2)
+    var pasco = EstacionClass.new("Pasco", "PA", [-34.6096459617052, -58.3984269918123], {}, {"CO": 1, "AL": 1}, false, 0)
+    var alberti = EstacionClass.new("Alberti", "AL", [-34.6098335784398, -58.401207534233], {}, {"PA": 1}, false, 0)
     
     # Creamos la línea A y asignamos el color
     var Linea_a = LineaClass.new("A", Color(0, 0.69, 0.87), {"Lunes": "05:30 - 22:53", "Martes": "05:30 - 22:53", "Miércoles": "05:30 - 22:53", "Jueves": "05:30 - 22:53", "Viernes": "05:30 - 22:53", "Sábado": "06:00 - 23:27", "Domingo": "08:00 - 22:01"}, {"05:30 - 22:53": 4, "06:00 - 23:27": 6, "08:00 - 22:01": 7})
