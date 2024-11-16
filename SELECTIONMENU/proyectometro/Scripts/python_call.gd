2aefe6ca7b30160c4dd5253772a81415479d49dd
wenjie198 <wenjie1987711563@gmail.com>
2024-11-16 14:31:14 +0100

    venv Python + python_call.gd fix

--- SELECTIONMENU/proyectometro/Scripts/python_call.gd
+++ SELECTIONMENU/proyectometro/Scripts/python_call.gd
@@ -1,26 +1,26 @@
 extends Node
 
-func get_python_path() -> String:
-	var command = "where" if OS.get_name() == "Windows" else "which"
-	var result = []
-	var res = OS.execute(command, ["python"], result, true)
-	
-	if res == 0 and result.size() > 0:
-		return result[0].strip_edges()
-	else:
-		print("Error: Python not found in system PATH.")
-		return ""
-	
-	
+var DIR = OS.get_executable_path().get_base_dir()
+var interpreter_path = DIR.path_join("Scripts/PythonFiles/venv/Scripts/python")
+var script_path = DIR.path_join("Scripts/PythonFiles/backend.py")
+
 func run_python_script(source_id, target_id):
-	var python_path = get_python_path()
+	# var python_path = get_python_path()
+	
+	if !OS.has_feature("template"):
+		print("B")
+		interpreter_path = ProjectSettings.globalize_path("res://Scripts/PythonFiles/venv/Scripts/python")
+		script_path = ProjectSettings.globalize_path("res://Scripts/PythonFiles/backend.py")
+	
+	var python_path = interpreter_path
 	print(python_path)
 	if python_path == "":
 		print("Error: Unable to find Python executable.")
 		return
 		
-	var output = []
-	var python_file = ProjectSettings.globalize_path("res://Scripts/backend.py")
+	var output = []	
+	#var python_file = ProjectSettings.globalize_path("res://Scripts/backend.py")
+	var python_file = script_path
 	var result = OS.execute(python_path, [python_file, str(source_id), str(target_id)], output, true, false)
 	
 	if result == 0:
